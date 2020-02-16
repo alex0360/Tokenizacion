fc0b597627af4be29a35aac68a5fe9e0f5f320f6
alex DS <32990752+alex0360@users.noreply.github.com>
2020-02-16 12:57:18 -0400

    C#

---
+++ PaymentProfile/PaymentProfile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Tokenizacion
+{
+    class PaymentProfile
+    {
+        //public string PrivateAccountKey ="9kYH2uY5zoTD-WBMEoc0KNRQYrC7crPRJ7zPegg3suXguw_8L-rZDQ__";
+        //public string PrivateAccountKey ="on3smurlSFA-_xT9IRGDv6v17bAY8Ri6acwsmjpjIojkNmByKuUJkA__";
+        public static string PaymentProfileActivation()
+        {
+            var client = new RestSharp.RestClient("https://lab.cardnet.com.do/servicios/tokens/v1/api/Customer/62057/activate") { Timeout = -1 };
+            var request = new RestSharp.RestRequest(RestSharp.Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Basic 9kYH2uY5zoTD-WBMEoc0KNRQYrC7crPRJ7zPegg3suXguw_8L-rZDQ__");
+
+            request.AddParameter("application/json", "{" +
+                                                        "\"Token\": \"CT___mI2B-hLk3nI--7Pyl8U6_VPYyA-8Za2M4Gj0WncLfg_\",\n"+
+                                                        "\"ActivationCode\": \"KWA94Z\"}", 
+                                RestSharp.ParameterType.RequestBody);
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        public static string PaymentProfileUpdate()
+        {
+            var client = new RestSharp.RestClient("https://lab.cardnet.com.do/servicios/tokens/v1/api/Customer/100/PaymentProfileUpdate") 
+            { Timeout = -1 };
+            var request = new RestSharp.RestRequest(RestSharp.Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Basic 9kYH2uY5zoTD-WBMEoc0KNRQYrC7crPRJ7zPegg3suXguw_8L-rZDQ__");
+
+            request.AddParameter("application/json", "{" +
+                                                        "\"PaymentProfileId\": \"345\",\n" + 
+                                                        "\"Expiration\": \"202108\",\n" +
+                                                        "\"Enabled\": true" +                            
+                                                     "}", RestSharp.ParameterType.RequestBody);
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        public static string PaymentProfileDelete()
+        {
+            var client = new RestSharp.RestClient("https://lab.cardnet.com.do/servicios/tokens/v1/api/Customer/100/PaymentProfileDelete") { Timeout = -1 };
+            var request = new RestSharp.RestRequest(RestSharp.Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Basic 9kYH2uY5zoTD-WBMEoc0KNRQYrC7crPRJ7zPegg3suXguw_8L-rZDQ__");
+
+            request.AddParameter("application/json", "{ \"PaymentProfileId\": \"1915\"}", RestSharp.ParameterType.RequestBody);
+
+            RestSharp.IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+    }
+}
