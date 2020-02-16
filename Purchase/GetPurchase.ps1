63d443cba5d57ff0955c22c754ee280e935e1b6a
alex DS <32990752+alex0360@users.noreply.github.com>
2020-02-16 12:50:15 -0400

    PowerShell
    Para correr en servidores Shell

---
+++ Purchase/GetPurchase.ps1
@@ -0,0 +1,6 @@
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+$headers.Add("Authorization", "Basic 9kYH2uY5zoTD-WBMEoc0KNRQYrC7crPRJ7zPegg3suXguw_8L-rZDQ__")
+
+$response = Invoke-RestMethod -Uri "https://lab.cardnet.com.do/servicios/tokens/v1/api/Purchase/12511"  -Method GET -Headers $headers -Body $body
+$response | ConvertTo-Json
